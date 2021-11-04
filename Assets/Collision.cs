94228def4e00f79537d46d57831e6a691ed8b71b
trihai264 <53052320+trihai264@users.noreply.github.com>
2021-11-04 13:17:27 +0200

    updated
    added assets and simple code

---
+++ Assets/Collision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
